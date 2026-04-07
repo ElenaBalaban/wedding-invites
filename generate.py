669031836ee34ca3e8939a3399424a8c4bac4b16
ybalaban <yarchick.balaban@gmail.com>
2026-04-07 18:47:27 -0400

    Добавлены гости 22-24 (Алексей+Юлия+Кирилл+Анастасия+Елизавета, Илья+Анна+Мая, Машенька)

--- generate.py
+++ generate.py
@@ -25,7 +25,7 @@ TRANSLIT = {
 }
 
 # Known single female names (for "Дорогая" instead of "Дорогой")
-FEMALE_NAMES = {"Наташенька", "Мамочка Лена"}
+FEMALE_NAMES = {"Наташенька", "Мамочка Лена", "Машенька"}
 
 
 def transliterate(text):
